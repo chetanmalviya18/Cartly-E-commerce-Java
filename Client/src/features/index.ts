a5c28b3fd95364ee8480a6c92f5d5fc0b4845958
chetanmalviya18 <chetanmalviya.182@gmail.com>
2026-08-21 13:31:57 +0530

    feat(client): restructure react client with scalable production architecture

---
+++ Client/src/features/index.ts
@@ -0,0 +1,6 @@
+export * from './auth';
+export * from './products';
+export * from './cart';
+export * from './checkout';
+export * from './orders';
+export * from './user';
---
+++ Client/src/pages/index.ts
@@ -0,0 +1,7 @@
+export * from './home';
+export * from './products';
+export * from './cart';
+export * from './checkout';
+export * from './orders';
+export * from './profile';
+export * from './not-found';
